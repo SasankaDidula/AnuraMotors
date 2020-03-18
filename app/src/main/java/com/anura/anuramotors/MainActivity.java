15d028470a38473515faa9b17d9f78bcf433584a
shanakaHasa <61416407+shanakaHasa@users.noreply.github.com>
2020-03-19 02:45:47 +0530

    comited regis

--- app/src/main/java/com/anura/anuramotors/MainActivity.java
+++ app/src/main/java/com/anura/anuramotors/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +24,19 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+        Button register = findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, registerUser.class));
+            }
+
+        });
+
+    }
+    public void onClick(View v) {
+        startActivity(new Intent(MainActivity.this, Appointments.class));
     }
 }
