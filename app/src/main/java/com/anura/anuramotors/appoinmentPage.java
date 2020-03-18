e40660e5a49a2a2edfbcbd91bf24ceaf9fb6dd89
lameshan <62106334+lameshan@users.noreply.github.com>
2020-03-18 16:11:59 +0530

    Revert "Merge remote-tracking branch 'origin/main' into main"
    This reverts commit d853570e8906028f900d8cf914658b75c52939a5, reversing
    changes made to e4cb2a94aaf2d700c29ee47aa421cffd00aa8832.

--- app/src/main/java/com/anura/anuramotors/appoinmentPage.java
+++ app/src/main/java/com/anura/anuramotors/appoinmentPage.java
@@ -2,10 +2,7 @@ package com.anura.anuramotors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class appoinmentPage extends AppCompatActivity {
 
@@ -13,25 +10,5 @@ public class appoinmentPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appoinment_page);
-
-        Button update = findViewById(R.id.update_btn);
-        update.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(appoinmentPage.this, AddNewAppoinment.class));
-            }
-
-        });
-
-        Button back = findViewById(R.id.back_btn);
-        back.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(appoinmentPage.this, MainActivity.class));
-            }
-
-        });
     }
 }
