ffcced0bf70b2099ed82cad1f1c061b39f7a508c
SasankaDidula <61312369+SasankaDidula@users.noreply.github.com>
2020-03-18 20:54:41 +0530

    Apppointment UI

--- app/src/main/java/com/anura/anuramotors/AddNewAppoinment.java
+++ app/src/main/java/com/anura/anuramotors/AddNewAppoinment.java
@@ -6,24 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-public class appoinmentPage extends AppCompatActivity {
+public class AddNewAppoinment extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_appoinment_page);
+        setContentView(R.layout.activity_add_new_appoinment);
+
+        Button add = findViewById(R.id.add_btn);
+        add.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AddNewAppoinment.this, Appointments.class));
+            }
+
+        });
 
         Button back = findViewById(R.id.back_btn);
         back.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(appoinmentPage.this, Appointments.class));
+                startActivity(new Intent(AddNewAppoinment.this, Appointments.class));
             }
 
         });
 
+        
     }
 }
--- app/src/main/java/com/anura/anuramotors/VehicleDetails.java
+++ app/src/main/java/com/anura/anuramotors/VehicleDetails.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Addvehicle extends AppCompatActivity {
+public class VehicleDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_addvehicle);
+        setContentView(R.layout.activity_vehicle_details);
     }
 }
