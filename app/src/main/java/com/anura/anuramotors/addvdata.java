5f4ab817594d4c58f46beaa98ae155465ad67b44
lameshan <>
2020-04-20 14:07:30 +0530

    add data

---
+++ app/src/main/java/com/anura/anuramotors/addvdata.java
@@ -0,0 +1,44 @@
+package com.anura.anuramotors;
+
+public class addvdata {
+    private String VNO;
+    private  String TYPE;
+    private  String COLOUR;
+    private  String DETAILS;
+
+    public addvdata() {
+    }
+
+    public String getVNO() {
+        return VNO;
+    }
+
+    public void setVNO(String VNO) {
+        this.VNO = VNO;
+    }
+
+    public String getTYPE() {
+        return TYPE;
+    }
+
+    public void setTYPE(String TYPE) {
+        this.TYPE = TYPE;
+    }
+
+    public String getCOLOUR() {
+        return COLOUR;
+    }
+
+    public void setCOLOUR(String COLOUR) {
+        this.COLOUR = COLOUR;
+    }
+
+    public String getDETAILS() {
+        return DETAILS;
+    }
+
+    public void setDETAILS(String DETAILS) {
+        this.DETAILS = DETAILS;
+    }
+}
+
