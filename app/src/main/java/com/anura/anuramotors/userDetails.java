d5ada30e51e62262d1e2ef03b066fa39a05af2ab
shanakaHasa <61416407+shanakaHasa@users.noreply.github.com>
2020-04-22 00:54:57 +0530

    user update function

--- app/src/main/java/com/anura/anuramotors/userDetails.java
+++ app/src/main/java/com/anura/anuramotors/userDetails.java
@@ -9,13 +9,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class userDetails extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
+import javax.annotation.Nullable;
+
+public class userDetails extends AppCompatActivity {
+    FirebaseAuth fAuth;
     private Context mContext;
     private TextView mName;
     private TextView mAddress;
     private TextView mMobile;
     private String key;
+    private FirebaseFirestore  db;
+
+//    private DocumentReference userRef = db.collection("User").document("phoneNumber");
 
 
 
@@ -24,10 +36,26 @@ public class userDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_details);
 
+
+
+
         mName = (TextView) findViewById(R.id.userName);
         mAddress = (TextView) findViewById(R.id.userAddress);
         mMobile = (TextView) findViewById(R.id.userMobile);
 
+        fAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        key = fAuth.getCurrentUser().getPhoneNumber();
+        DocumentReference documentReference = db.collection("User").document(key);
+
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                mName.setText(documentSnapshot.getString("name"));
+                mAddress.setText(documentSnapshot.getString("address"));
+                mMobile.setText(documentSnapshot.getString("phoneNumber"));
+            }
+        });
         Button edit_r_btn = findViewById(R.id.edit_r_btn);
         edit_r_btn.setOnClickListener(new View.OnClickListener() {
 
--- app/src/main/java/com/anura/anuramotors/userEdit.java
+++ app/src/main/java/com/anura/anuramotors/userEdit.java
@@ -1,19 +1,75 @@
 package com.anura.anuramotors;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.anura.anuramotors.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
 
 public class userEdit extends AppCompatActivity {
 
+    private EditText uName, uAddress, uMobile;
+    private Button save;
+    private FirebaseAuth fAuth;
+    private FirebaseDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_edit);
 
+        uName = findViewById(R.id.name_e);
+        uAddress = findViewById(R.id.address_e);
+        uMobile = findViewById(R.id.mNo_e);
+        save = findViewById(R.id.update_e);
+
+        fAuth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+
+
+        DatabaseReference dbRef = db.getReference(fAuth.getUid());
+
+
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                User user = dataSnapshot.getValue(User.class);
+                uName.setText(user.getName());
+                uAddress.setText(user.getAddress());
+                uMobile.setText(user.getPhoneNumber());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                Toast.makeText(userEdit.this, databaseError.getCode(), Toast.LENGTH_LONG).show();
+            }
+        });
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = uName.getText().toString();
+                String address = uAddress.getText().toString();
+                String mobile = uMobile.getText().toString();
+
+                User user = new User(name, address, mobile);
+                dbRef.setValue(user);
+                finish();
+            }
+        });
         Button back = findViewById(R.id.back_e);
         back.setOnClickListener(new View.OnClickListener() {
 
